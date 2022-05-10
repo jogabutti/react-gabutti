b169f48a72d19da3b5454ad7d4bc73db55ca6f2f
Maria Jose <jogabutti@gmail.com>
2022-05-10 15:17:51 -0300

    2ff

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit and save to reload.
+          Edit and saveasdas dddto reload.
         </p>
       </header>
     </div>
