650f6c74221ed96030227e90398dca374b0c3d34
Maria Jose <jogabutti@gmail.com>
2022-05-10 15:23:01 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit and saveasdas dddto reload.
+          Edit and savegasdfsdsdas dddto reload.
         </p>
       </header>
     </div>
